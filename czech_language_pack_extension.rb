89489dc274bd4799cfff91d41fb662af44a708e3
Michal Cichra <michal.cichra@gmail.com>
2010-08-08 17:13:39 +0200

    few translated messages

--- czech_language_pack_extension.rb
+++ czech_language_pack_extension.rb
@@ -1,7 +1,7 @@
-class FrenchLanguagePackExtension < Radiant::Extension
+class CzechhLanguagePackExtension < Radiant::Extension
   version "1.0"
-  description "Provides a French translation for the Radiant admin interface"
-  url "http://yourwebsite.com/fr"
+  description "Provides a Czech translation for the Radiant admin interface"
+  url "http://github.com/mikz/radiant-czech_language_pack-extension/"
   
   def activate
 
