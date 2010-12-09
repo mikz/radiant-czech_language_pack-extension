3ccb270c126eed48a5b06bf81a84d12349a14a7f
Michal Cichra <michal.cichra@gmail.com>
2010-12-10 00:34:15 +0100

    fixed typo

--- czech_language_pack_extension.rb
+++ czech_language_pack_extension.rb
@@ -1,4 +1,4 @@
-class CzechhLanguagePackExtension < Radiant::Extension
+class CzechLanguagePackExtension < Radiant::Extension
   version "1.0"
   description "Provides a Czech translation for the Radiant admin interface"
   url "http://github.com/mikz/radiant-czech_language_pack-extension/"
